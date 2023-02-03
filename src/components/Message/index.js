eac181419fc4e62d098fe4a58be722997bdc07d9
saranshkhulbe7 <saranshkhulbe7@gmail.com>
2023-02-03 08:56:17 +0530

    message shadows

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -26,6 +26,15 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
     maxWidth: "80%",
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
   },
   time: {
     color: "gray",
